04b2068080f74639a1cd4053158429dfbd1fc47f
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2019-05-12 23:06:40 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,33 @@
+In this challenge, we're getting started with conditional statements. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+
+####################################################################################
+My Code..........
+n = int(input())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
