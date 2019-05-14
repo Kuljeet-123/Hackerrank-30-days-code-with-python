eaf9ca705c9b6e96cbbeeb716fe4d5e08223c5f8
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2019-05-15 00:12:52 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,22 @@
+In this challenge, we're going to use loops to help us do some simple math. Check out the Tutorial tab to learn more.
+
+Task 
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print  lines of output; each line  (where ) contains the  of  in the form: 
+n x i = result.
+
+
+########################################################################################################
+My Code...........
+t=int(input())
+for i in range(1,11):
+    print (t, "x", i, "=", (t*i))
