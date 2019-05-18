b120f981dc633de4a5887c3e0a088fdf7408ea2f
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2019-05-19 00:03:01 +0530

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,39 @@
+Write a factorial function that takes a positive integer,  as a parameter and prints the result of  ( factorial).
+
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of .
+
+Input Format
+
+A single integer,  (the argument to pass to factorial).
+
+Constraints
+
+Your submission must contain a recursive function named factorial.
+Output Format
+
+Print a single integer denoting .
+
+
+
+#####################################################################################################
+My Code...........................
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    result = factorial(n)
+    print(result)
+     
