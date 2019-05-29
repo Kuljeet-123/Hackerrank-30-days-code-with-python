a0308406adf1253d59cbb60ce5ac598ce5f1e7a8
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2019-05-29 14:17:04 +0530

    Create day-19.py

---
+++ day-19.py
@@ -0,0 +1,34 @@
+The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of .
+
+Input Format
+
+A single line containing an integer, .
+
+Constraints
+
+Output Format
+
+You are not responsible for printing anything to stdout. The locked template code in the editor below will call your code and print the necessary output.
+###########################################################################################################################
+My Code......................................
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        divisor_sum = 0
+        for i in range(1, n+1):
+            if n%i == 0:
+                divisor_sum += i
+        return divisor_sum
+
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
